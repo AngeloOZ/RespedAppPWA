0f030ce54792c4d1a67292c9091e664162ab9c26
AngeloOz <angelo-mjz7@hotmail.com>
2022-06-29 11:50:53 -0500

    feat: pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+/* @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')
 
-module.exports = nextConfig
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public'
+  },
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  }
+})
+
+// const nextConfig = {
+//   reactStrictMode: true,
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   }
+// }
+
+// module.exports = nextConfig
